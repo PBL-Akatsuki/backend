bb8ffd6a7a6272262277097e9d149c2df14e723d
Vedant-Mhatre <vedantmh@gmail.com>
2024-12-17 01:10:02 +0530

    removed unnecessary old code

--- app/database.py
+++ app/database.py
@@ -2,20 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.sql import text
-
-# from dotenv import load_dotenv
 import os
-
-# load_dotenv()
-
-# # DATABASE_URL = os.getenv("DATABASE_URL")
-# DATABASE_URL = "postgresql://user:password@postgres:5432/mydatabase"
-
-# engine = create_engine(DATABASE_URL)
-
-# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Base = declarative_base()
 
 # Get environment or use default
 DATABASE_URL = os.getenv(
